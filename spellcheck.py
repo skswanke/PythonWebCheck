22b468f62fed05415e68fa71a7d55ddd97d671c3
skswanke <skswanke@gmail.com>
2015-09-15 19:45:06 -0400

    spellcheck updates

--- spellcheck.py
+++ spellcheck.py
@@ -33,3 +33,50 @@ def visible(element):
     elif re.match('<!--.*-->', str(element)):
         return False
     return True
+
+
+'''
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import urllib.request
+import sys
+import re
+import string
+
+def checktext():
+    pageFile = urllib.request.urlopen("http://www.uvm.edu/~cems/")
+    pageHTML = pageFile.read()
+    pageFile.close()
+    soup = BeautifulSoup(pageHTML)
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+    text.encode(sys.stdout.encoding, errors='replace')
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    text = re.sub(r'\w*\d\w*', '', str(text)).strip()
+    uprint(str(text))
+    findwords = re.compile(r"\b[A-Za-z][A-Za-z][A-Za-z]+\b")
+    words = findwords.findall(text)
+    print(words)
+
+
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
+
+
+checktext()
+'''
