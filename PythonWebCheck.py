f3e83f4aa0c564dfd973dfdd5836ce61d4978193
skswanke <skswanke@gmail.com>
2015-09-21 17:00:34 -0400

    Clean The Fluff, Spell check not working

--- PythonWebCheck.py
+++ PythonWebCheck.py
@@ -8,18 +8,18 @@ from queue import Queue
 from threading import Thread
 
 # This is the start point for the recursion
-BASEURL = "http://www.uvm.edu/~cems/"
+BASEURL = "http://www.uvm.edu/~cems/?Page=explore/default.php&SM=explore/_exploremenu.html"
 
 # This is what would make the link bad (to the wrong place)
 CHECK = "sandbox"
 
 # This limits the iteration to links containing this phrase
-REPEAT = "uvm.edu/~cems/"
+REPEAT = "http://www.uvm.edu/~cems/?Page=explore/default.php&SM=explore/_exploremenu.html"
 
 # This helps optimise by ignoring links with these phrases
 EXCEPT = ['magic','.pdf','calendar','#bannermenu','#local', \
     '#uvmmaincontent','cems&howmany','.jpg', 'Page=Courses', \
-    'mailto:', 'tel:']
+    'mailto:', 'tel:', '#', '.zip']
 
 # This prevents repitition in the recursion 
 CHECKEDLINKS = set()
@@ -43,9 +43,9 @@ if (SPELLCHECK):
 BASECAMP = False
 if (BASECAMP):
     from handleAPI import sendToBaseCamp
-# Your Basecamp Username and Password
-USERNAME = ""
-PASSWORD = ""
+    # Your Basecamp Username and Password
+    USERNAME = ""
+    PASSWORD = ""
 
 # This will check ALL links for 404 It will take Significantly more time
 CHECK404 = False
@@ -70,11 +70,9 @@ BADLINKS = []
 
 urlsToVisit = Queue()
 
-THREADS = 6
+#The number of threads to operate in
+THREADS = 1
 
-'''
-This function runs the file operations and starts the other functions
-'''
 def main():
     global BADLINKS
     global SPELLINGERRORS
@@ -140,15 +138,7 @@ def main():
     print("Finished {}".format(datetime.now() - ts))
     print(len(CHECKEDLINKS))
 
-def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
-    enc = file.encoding
-    if enc == 'UTF-8':
-        print(*objects, sep=sep, end=end, file=file)
-    else:
-        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
-        print(*map(f, objects), sep=sep, end=end, file=file)
-    
-
+# Where the magic happens
 class DownloadWorker(Thread):
     def __init__(self, queue, i):
         Thread.__init__(self)
@@ -170,7 +160,7 @@ class DownloadWorker(Thread):
                     soup = BeautifulSoup(page.text, "html.parser")
                     links = soup.find_all('a')
                     if (SPELLCHECK):
-                        checked = spellcheck(soup, newlink)
+                        checked = spellcheck(soup, newlink, SLACK)
                     if (checked):
                         BADLINKS.append(checked)
                         SPELLINGERRORS.append(checked)
@@ -215,155 +205,21 @@ class DownloadWorker(Thread):
 
             urlsToVisit.task_done()
 
-
-'''
-@params:
-    Base url
-    last url (blank if none)
-
-@returns:
-    bLinks: a list of discovered bad links including 
-        the links referer
-
-This function recursively crawls an entire sitemap by 
-following every link with the same base url on every
-page on the site.
-
-It uses 
-    -BASEURL as a start point
-    -CHECK to check the links against for an error
-    -REPEAT to limit where it recurses
-    -EXCEPT to optimize against repetitive/unwanted links
-    -CHECKEDLINKS to prevent repetition
-'''
-'''
-def getBadLinks(url, lastUrl):
-    try:
-        bLinks = []
-        """        
-        page = requests.get(url)
-        pageHTML = page.text
-
-        global COUNT
-        global CHECKEDLINKS
-
-        COUNT += 1
-        
-        soup = BeautifulSoup(pageHTML, "html.parser")
-        linkSoup = soup.find_all('a')
-        
-        if (SPELLCHECK):
-            checked = spellcheck(soup, url)
-            if (checked):
-                bLinks.append(checked)
-                SPELLINGERRORS.append(checked)
-        """
-        urlstovisit = Queue()
-        urlstovisit.put(url, lastUrl)
-        
-        while (urlstovisit.qsize() > 0):
-            print (urlstovisit.qsize())
-            nexturl = urlstovisit.get()
-            print(nexturl)
-            page = requests.get(nexturl)
-            pageHTML = page.text
-
-            global COUNT
-            global CHECKEDLINKS
-
-            COUNT += 1
-            
-            soup = BeautifulSoup(pageHTML, "html.parser")
-            linkSoup = soup.find_all('a')
-            print(len(linkSoup))
-            #process request
-            CHECKEDLINKS.add(nexturl)
-            if (SPELLCHECK):
-                checked = spellcheck(soup, url)
-            if (checked):
-                bLinks.append(checked)
-                SPELLINGERRORS.append(checked)
-            for link in linkSoup:
-                print (urlstovisit.qsize())
-                #add links to queue
-                if link.has_attr('href'):
-                    link['href'] = urlReconstruct(url, link['href'])
-                    if (CHECK in link['href']):
-                        if (SLACK):
-                            bLinks.append('Bad Link in <'+url+'|link> linking to: <'+link['href'] + '|link>')
-                        else:
-                            bLinks.append('Bad Link in '+url+'\nlinking to: '+link['href'])
-                        if (DEBUG):
-                            print('foundbadlink: '+url)
-                    elif (REPEAT in link['href'] and not any(x in link['href'] for x in EXCEPT)):
-                        if (link['href'] not in CHECKEDLINKS):
-                            CHECKEDLINKS.add(link['href'])
-                            urlstovisit.put(link['href'],url)
-                        elif (link['href'] not in CHECKEDLINKS):
-                            CHECKEDLINKS.add(link['href'])
-                            urlstovisit.put(link)
-                    elif (CHECK404 and link['href'] not in CHECKEDLINKS and 404 == getResponseCode(link['href'])):
-                        CHECKEDLINKS.add(link['href'])
-                        if (SLACK):
-                            bLinks.append(["404 in page: <" + url + "|link> Linking to: <" + link['href'] + "|link>"])
-                        else:
-                            bLinks.append(["404 in page: " + url + "\nLinking to: " + link['href']])
-            urlstovisit.task_done()
-            return blinks
-
-        """
-        for link in linkSoup:
-            if link.has_attr('href'):
-                link['href'] = urlReconstruct(url, link['href'])
-                if (CHECK in link['href']):
-                    if (SLACK):
-                        bLinks.append('Bad Link in <'+url+'|link> linking to: <'+link['href'] + '|link>')
-                    else:
-                        bLinks.append('Bad Link in '+url+'\nlinking to: '+link['href'])
-                    if (DEBUG):
-                        print('foundbadlink: '+url)
-                elif (REPEAT in link['href'] and not any(x in link['href'] for x in EXCEPT)):
-                    if (link['href'] not in CHECKEDLINKS):
-                        CHECKEDLINKS.append(link['href'])
-                        bLinks.extend(getBadLinks(link['href'],url))
-                    elif (link['href'] not in CHECKEDLINKS):
-                        CHECKEDLINKS.append(link['href'])
-                        if('http' not in link['href']):
-                            bLinks.extend(getBadLinks('http:' + URLSTART + link['href'], url))
-                        else:
-                            bLinks.extend(getBadLinks(URLSTART+link['href'],url))
-                elif (CHECK404 and link['href'] not in CHECKEDLINKS and 404 == getResponseCode(link['href'])):
-                    CHECKEDLINKS.append(link['href'])
-                    if (SLACK):
-                        bLinks.append(["404 in page: <" + url + "|link> Linking to: <" + link['href'] + "|link>"])
-                    else:
-                        bLinks.append(["404 in page: " + url + "\nLinking to: " + link['href']])
-
-        COUNT -= 1
-
-        return bLinks
-        """
-    except Exception as e:
-        raise e
-        if ("404" in str(e)):
-            if (DEBUG):
-                print("404 in page: " + lastUrl + "\nLinking to: " + url)
-            if (SLACK):
-                return ["404 in page: <" + lastUrl + "|link> Linking to: <" + url + "|link>"]
-            return ["404 in page: " + lastUrl + "\nLinking to: " + url]
-        elif ("href" in str(e)):
-            if (DEBUG):
-                print("Error: href")
-            pass
-        else:
-            if (DEBUG):
-                print('Error: ' + str(e))
-            pass
-        return bLinks
-'''
+# This will reconstruct a relative url
 def urlReconstruct(base, url):
+    #print (base + " + " + url)
+    #print (str(urljoin(str(base.replace("https", "http")), str(url.replace("https", "http")))))
     return str(urljoin(str(base.replace("https", "http")), str(url.replace("https", "http"))))
 
+# This function will print anything (no matter the encoding)
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
+    
 
 # Run the program
 main()
@@ -373,8 +229,6 @@ if (DEBUG):
     print("Checked Links: " + str(len(CHECKEDLINKS)))
     print(str(finished.hour) + ':' + str(finished.minute) + ':' + str(finished.second))
     print("Time elapsed: " + str(finished - DATE))
-
-if(DEBUG):
     file = open('Logs/Checkedlinks.txt', 'w+')
     c = 0
     d = []
--- spellcheck.py
+++ spellcheck.py
@@ -24,27 +24,45 @@ class SpellCheck():
         spellwords = map(lambda x: x.strip(), spellwords)
         for word in spellwords : self.hash[word] = True
 
-    def checktext(self, soup, url):
+    def checktext(self, soup, url, SLACK):
+        print("++--Soup.get_text()--++")
+        self.uprint(soup)
         # kill all script and style elements
         for script in soup(["script", "style"]):
             script.extract()    # rip it out
 
         # get text
         text = soup.get_text()
-        #text = text.encode(sys.stdout.encoding, errors='replace')
+        print("++--script.extract()--++")
+        self.uprint(soup)
+        print("++--Get_Text()--++")
+        self.uprint(soup.get_text())
         # break into lines and remove leading and trailing space on each
         lines = (line.strip() for line in text.splitlines())
+        print("++--Lines--++")
+        self.uprint(lines)
         # break multi-headlines into a line each
         chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        print("++--Chunks--++")
+        self.uprint(chunks)
         # drop blank lines
         text = '\n'.join(chunk for chunk in chunks if chunk)
+        print("++--Joined Chunks--++")
+        self.uprint(text)
         text = re.sub(r'\w*\d\w*', '', str(text)).strip()
-        text = re.sub(r'\w*([a-zA-Z])[\d.]([a-zA-Z]).*\w*', '', str(text)).strip()
+        print("++--Regex 1--++")
+        self.uprint(text)
+        text = re.sub(r'\w+[a-zA-Z0-9.+-][\d\.\/\@\*-][a-zA-Z.@/?=0-9]*\w+', '', str(text)).strip()
+        print("++--Regex 2--++")
+        self.uprint(text)
         findwords = re.compile(r"\b[A-Za-z][A-Za-z][A-Za-z]+\b")
         words = findwords.findall(text)
-        #print(words)
+        print("++--Words--++")
+        self.uprint(words)
         badwords = self.spellCheckHash(words)
         if badwords:
+            if SLACK:
+                return "Spelling Errors on page: <" + url + "|Link> :: " + str(badwords)
             return "Spelling Errors on page: " + url + " :: " + str(badwords)
         else:
             return False
@@ -60,3 +78,11 @@ class SpellCheck():
         #         print(i)
         #print(misspelled)
         return seterrors
+    
+    def uprint(self, *objects, sep=' ', end='\n', file=sys.stdout):
+        enc = file.encoding
+        if enc == 'UTF-8':
+            print(*objects, sep=sep, end=end, file=file)
+        else:
+            f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+            print(*map(f, objects), sep=sep, end=end, file=file)
