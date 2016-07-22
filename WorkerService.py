dfec26ae2aed6dea66c1147f9eb2897e4a768c1a
skswanke <skswanke@users.noreply.github.com>
2016-07-22 12:18:56 -0400

    Create WorkerService.py
    Extracted methods into a service

---
+++ WorkerService.py
@@ -0,0 +1,12 @@
+# This will reconstruct a relative url or return an absolute url unchanged
+def urlReconstruct(base, url):
+    return str(urljoin(str(base.replace("https", "http")), str(url.replace("https", "http"))))
+
+# Creates a new request to get the status (very slow)
+def getResponseCode(url):
+    try:
+        r = requests.get(url)
+        return r.status_code == "404"
+    except e:
+        print(e)
+        return False
